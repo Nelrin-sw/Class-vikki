e5e7df1828dfaf48a25a94969e2b1576cc3acaba
Nelrin-sw <pronelrin.kumar.30v@gmail>
2020-09-09 18:10:48 +0530

    th

---
+++ tut10.js
@@ -0,0 +1,18 @@
+// Genrating loop
+
+for (let i = 5; i < 10; i++) {
+    if (i === 1) {
+        console.log(i*1);
+        console.log(i*2);
+        console.log(i*3);
+        console.log(i*4);
+        console.log(i*5);
+        console.log(i*6);
+        console.log(i*7);
+        console.log(i*8);
+        console.log(i*9);
+        console.log(i*10);
+        break
+    }
+    console.log(i)
+}
